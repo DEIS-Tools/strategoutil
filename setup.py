71e05b18c0effc80d30fc03080e8652153d5f6ac
mihsamusev <mih.samusev@gmail.com>
2021-05-31 17:39:49 +0200

    testing releases

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
-from distutils.core import setup
-from setuptools import find_packages
+import setuptools
 
-setup(
-    name="strategoutil",
-    packages=find_packages(),
-    version="0.0.1",
-    license="MIT",
-    description="Python utility functions library for UPPAAL Stratego",
-    author="Mihhail Samusev",
-    url="https://github.com/mihsamusev",
-    keywords=["uppaal", "stratego"]
-)
+# for compatiability with editable installs 
+# pip install -e
+
+if __name__ == "__main__":
+    setuptools.setup()
