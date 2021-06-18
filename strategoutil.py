06ae8e59d9726afcc7e68bc8eb2ec66f6da1aa43
Martijn Goorden <m.a.goorden@tue.nl>
2021-06-18 12:17:33 +0200

    Update documentation of the StrategoController class

--- strategoutil.py
+++ strategoutil.py
@@ -155,12 +155,30 @@ def print_progress_bar(i, max, postText):
 class StrategoController:
     """
     Controller class to interface with UPPAAL Stratego through python.
+
+    :param modeltemplatefile: The file name of the template model.
+    :type modeltemplatefile: str
+    :param model_cfg_dict: Dictionary containing pairs of state variable name and its initial
+        value. The state variable name should match the tag name in the template model.
+    :type model_cfg_dict: dict
+    :param cleanup: Whether or not to clean up the temporarily simulation file after being used.
+    :type cleanup: bool
+    :param interactive_bash: Wether or not to run Uppaal Stratego with interactive bash. Interactive
+        bash uses `.bashrc`, such that the user's aliases are available.
+    :type interactive_bash: bool
+    :ivar states: Dictionary containing the current state of the system, where a state is a pair of
+        variable name and value. It is initialized with the values from *model_cfg_dict*.
+    :vartype states: bool
+    :ivar tagRule: The rule for each tag in the template model. Currently, the rul is set to be
+        ``"//TAG_{}"``. Therefore, tags in the template model should be ``"//TAG_<variable name>"``,
+        where ``<variable name>`` is the global name of the variable.
+    :vartype tagRule: str
     """
 
     def __init__(self, modeltemplatefile, model_cfg_dict, cleanup=True, interactive_bash=True):
         self.templatefile = modeltemplatefile
         self.simulationfile = modeltemplatefile.replace(".xml", "_sim.xml")
-        self.cleanup = cleanup
+        self.cleanup = cleanup # TODO: this variable seems to be not used. Can it be safely removed?
         self.states = model_cfg_dict
         self.interactive_bash = interactive_bash
         self.tagRule = "//TAG_{}"
@@ -180,12 +198,18 @@ class StrategoController:
     def debug_copy(self, debug_file):
         """
         Copy UPPAAL simulationfile.xml file for manual debug in Stratego.
+
+        :param debug_file: The file name of the debug file.
+        :type debug_file: str
         """
         shutil.copyfile(self.simulationfile, debug_file)
 
     def update_state(self, new_values):
         """
         Update the state of the MPC controller.
+
+        :param new_values: Dictionary containing new values for the state variables.
+        :type new_values: dict
         """
         for name, value in new_values.items():
             self.states.update({name: value})
@@ -193,7 +217,7 @@ class StrategoController:
     def insert_state(self):
         """
         Insert the current state values of the variables at the appropriate position in the
-        simulation \*.xml file indicated by the tag rule.
+        simulation \*.xml file indicated by the :py:attr:`tagRule`.
         """
         for name, value in self.states.items():
             tag = self.tagRule.format(name)
@@ -202,6 +226,9 @@ class StrategoController:
     def get_var_names_as_string(self):
         """
         Print the names of the state variables separated by a ','.
+
+        :return: All the variable names joined together with a ','.
+        :rtype: str
         """
         separator = ","
         return separator.join(self.states.keys())
@@ -209,6 +236,9 @@ class StrategoController:
     def get_state_as_string(self):
         """
         Print the values of the state variables separated by a ','.
+
+        :return: All the variable values joined together with a ','.
+        :rtype: str
         """
         separator = ","
         values_as_string = [str(val) for val in self.states.values()]
@@ -216,24 +246,45 @@ class StrategoController:
 
     def get_state(self, key):
         """
-        Get the current value of the provided state.
+        Get the current value of the provided state variable.
+
+        :param key: The state variable name.
+        :type key: str
+        :return: The currently stored value of the state variable.
+        :rtype: int or float
         """
         return self.states.get(key)
 
     def get_states(self):
         """
-        Get the state variable names.
+        Get the current states.
+
+        :return: The current state dictionary.
+        :rtype: dict
         """
         return self.states
 
-    def run(self, queryfile="", learning_args=None, verifyta_path="verifyta"):
+    def run(self, queryfile="", learning_args=None, verifyta_command="verifyta"):
         """
         Runs verifyta with requested queries and parameters that are either part of the \*.xml model
         file or explicitly specified.
+
+        :param queryfile: The file name of the query file where the queries are written to.
+        :type queryfile: str
+        :param learning_args: Dictionary containing the learning parameters and their values. The
+            learning parameter names should be those used in the command line interface of Uppaal
+            Stratego. You can also include non-learning command line parameters in this dictionary.
+            If a non-learning command line parameter does not take any value, include the empty
+            string ``""`` as value.
+        :type learning_arg: dict
+        :param verifyta_command: The command name for running Uppaal Stratego at the user's machine.
+        :type verifyta_command: str
+        :return: The output generated by Uppaal Stratego.
+        :rtype: str
         """
         learning_args = {} if learning_args is None else learning_args
         output = run_stratego(self.simulationfile, queryfile,
-                              learning_args, verifyta_path, self.interactive_bash)
+                              learning_args, verifyta_command, self.interactive_bash)
         return output[0]
 
 
@@ -258,8 +309,8 @@ class MPCsetup:
         a non-learning command line parameter does not take any value, include the empty string ``""``
         as value.
     :type learning_arg: dict
-    :param verifytacommand: The command name for running Uppaal Stratego at the user's machine.
-    :type verifytacommand: str
+    :param verifyta_command: The command name for running Uppaal Stratego at the user's machine.
+    :type verifyta_command: str
     :param debug: Whether or not to run in debug mode.
     :type debug: bool
     :param interactive_bash: Wether or not to run Uppaal Stratego with interactive bash. Interactive
@@ -270,13 +321,13 @@ class MPCsetup:
     """
 
     def __init__(self, modeltemplatefile, output_file_path=None, queryfile="", model_cfg_dict=None,
-                 learning_args=None, verifytacommand="verifyta", debug=False, interactive_bash=True):
+                 learning_args=None, verifyta_command="verifyta", debug=False, interactive_bash=True):
         self.modeltemplatefile = modeltemplatefile
         self.output_file_path = output_file_path
         self.queryfile = queryfile
         self.model_cfg_dict = {} if model_cfg_dict is None else model_cfg_dict
         self.learning_args = {} if learning_args is None else learning_args
-        self.verifytacommand = verifytacommand
+        self.verifyta_command = verifyta_command
         self.debug = debug
         self.controller = StrategoController(self.modeltemplatefile, self.model_cfg_dict,
                                              interactive_bash=interactive_bash)
@@ -378,7 +429,7 @@ class MPCsetup:
             :meth:`~SafeMPCSetup.run_verifyta` in :class:`~SafeMPCSetup`.
         """
         result = self.controller.run(queryfile=self.queryfile, learning_args=self.learning_args,
-                                     verifyta_path=self.verifytacommand)
+                                     verifyta_path=self.verifyta_command)
 
         if self.controller.cleanup:
             self.controller.remove_simfile()
@@ -462,12 +513,12 @@ class SafeMPCSetup(MPCsetup):
             original method.
         """
         result = self.controller.run(queryfile=self.queryfile, learning_args=self.learning_args,
-                                     verifyta_path=self.verifytacommand)
+                                     verifyta_path=self.verifyta_command)
 
         if not successful_result(result):
             self.create_alternative_query_file(horizon, controlperiod, final)
             result = self.controller.run(queryfile=self.queryfile, learning_args=self.learning_args,
-                                         verifyta_path=self.verifytacommand)
+                                         verifyta_path=self.verifyta_command)
 
         if self.controller.cleanup:
             self.controller.remove_simfile()
