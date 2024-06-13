9d52bc1f6e9675d2e444c595f98bc653a4b33ef7
Martijn <mgoorden@cs.aau.dk>
2024-06-13 12:44:50 +0200

    Add double quotes around file paths to deal with spaces

--- strategoutil.py
+++ strategoutil.py
@@ -188,8 +188,8 @@ def run_stratego(model_file, query_file="", learning_args=None, verifyta_command
     learning_args = {} if learning_args is None else learning_args
     args = {
         "verifyta": verifyta_command,
-        "model": model_file,
-        "query": query_file,
+        "model": f'"{model_file}"',  # Robust against spaces in path name
+        "query": f'"{query_file}"',
         "config": merge_verifyta_args(learning_args)
     }
     args_list = [v for v in args.values() if v != ""]
